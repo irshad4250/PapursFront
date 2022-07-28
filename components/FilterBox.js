2e9f737464d0e3077960aab9d02aefab7ff10580
irshad <irshadbhoonee4250@gmail.com>
2022-07-28 13:52:08 +0400

    Fixed errors 1

--- components/FilterBox.js
+++ components/FilterBox.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import styles from "../styles/Components/Filterbox.module.css"
+import styles from "../styles/components/Filterbox.module.css"
 import { motion } from "framer-motion"
 import * as globalFunctions from "../Global/functions"
 
