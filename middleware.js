b70290d9b7b1bb1bea7f5011347fc306a987e098
irshad <irshadbhoonee4250@gmail.com>
2022-07-28 14:56:49 +0400

    error 7

--- middleware.js
+++ middleware.js
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server"
 import { makeId } from "./Global/functions"
 
-export function middleware(request, event) {
+export function middleware(request) {
+  console.log("Request")
+
   const response = NextResponse.next()
   const pidCookie = request.cookies.get("papursId")
 
