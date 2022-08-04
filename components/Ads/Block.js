7785bff7da40eb0421d5eb7ed7bdb96a4dafa492
irshad <irshadbhoonee4250@gmail.com>
2022-08-04 08:16:33 +0400

    ad block

--- components/Ads/Block.js
+++ components/Ads/Block.js
@@ -8,16 +8,14 @@ function Block() {
   }, [])
 
   return (
-    <div className="divMargin">
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-client="ca-pub-8618691594432056"
-        data-ad-slot="9996068388"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-    </div>
+    <ins
+      className="adsbygoogle"
+      style={{ display: "block" }}
+      data-ad-client="ca-pub-8618691594432056"
+      data-ad-slot="9996068388"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
   )
 }
 
