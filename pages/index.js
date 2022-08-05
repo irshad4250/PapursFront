833cc525778f0a048c82f271340610fee4f409e0
irshad <irshadbhoonee4250@gmail.com>
2022-08-05 18:25:28 +0400

    search finale

--- pages/index.js
+++ pages/index.js
@@ -179,7 +179,7 @@ export default function Home() {
           <div
             className={styles.searchButton}
             onClick={() => {
-              go
+              go()
             }}
           >
             Search
